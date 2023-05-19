b003932dbc119d092732158d97c9eff5d19adb28
rivka-yuliia <juliacreadora@gmail.com>
2023-05-19 17:40:40 +0300

    bonus task hash

--- hash_tables/first_recurring_character.py
+++ hash_tables/first_recurring_character.py
@@ -15,9 +15,24 @@ def first_recurring(array: list):
     return None
 
 
-array_1 = [1, 2, (5, 6), [], 2, 7]  # => 2
-array_2 = [5, 1, 1, 6, 9, 2]  # => 1
+# Bonus task
+def first_recurring2(array: list):
+    for i in range(len(array)-1):
+        item_1 = array[i]
+        for j in range(i+1, len(array)):
+            item_2 = array[j]
+            if item_2 == item_1:
+                inner = first_recurring2(array[i+1:j])
+                return inner if inner else item_2
+    return None
+
+
+array_1 = [1, 2, 6, 5, 2, 7]  # => 2
+array_2 = [5, 1, 1, 5, 9, 2]  # => 1
 array_3 = [1, 2, 3, 4]  # => None
 print(first_recurring(array_1))
+print(first_recurring2(array_1))
 print(first_recurring(array_2))
+print(first_recurring2(array_2))
 print(first_recurring(array_3))
+print(first_recurring2(array_3))
