0eeb71064a401b9f3eb3ec1852740104b1263980
rivka-yuliia <juliacreadora@gmail.com>
2023-05-21 12:25:24 +0300

    prepend for LL

--- linked_lists/LL_implementation.py
+++ linked_lists/LL_implementation.py
@@ -25,8 +25,18 @@ class LinkedList:
         self.length += 1
         return True
 
+    def prepend(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+        return True
+
 
 my_linked_list = LinkedList(10)
 my_linked_list.append(5)
 my_linked_list.append(16)
+my_linked_list.print_ll()
+
+my_linked_list.prepend(1)
 my_linked_list.print_ll()
