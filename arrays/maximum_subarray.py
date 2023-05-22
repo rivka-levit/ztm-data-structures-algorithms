c18e1fab717fcee09d48c2449c11b35532ef59df
rivka-yuliia <juliacreadora@gmail.com>
2023-05-22 07:34:15 +0300

    max subarray O(n) solution

--- arrays/maximum_subarray.py
+++ arrays/maximum_subarray.py
@@ -1,10 +1,9 @@
+# O(n^2)
 def max_sub_array(nums: list[int]) -> int:
     if not nums:
         return 0
-    if len(nums) == 1:
-        return nums[0]
 
-    max_sum = 0
+    max_sum = nums[0]
 
     for i in range(len(nums)-1):
         current_sum = nums[i]
@@ -19,10 +18,24 @@ def max_sub_array(nums: list[int]) -> int:
     return max_sum
 
 
+# O(n)
+def max_sub_array_2(nums: list[int]) -> int:
+    if not nums:
+        return 0
+
+    max_sum = current_sum = nums[0]
+
+    for n in nums[1:]:
+        current_sum = max((current_sum + n), n)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
 input_1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]  # --> 6
 input_2 = [1]  # --> 1
 input_3 = [5, 4, -1, 7, 8]  # --> 23
 
-print(max_sub_array(input_1))
-print(max_sub_array(input_2))
-print(max_sub_array(input_3))
+print(max_sub_array_2(input_1))
+print(max_sub_array_2(input_2))
+print(max_sub_array_2(input_3))
