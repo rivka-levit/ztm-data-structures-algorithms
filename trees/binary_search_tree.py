dd27320a0564e5a5c92802a5d08a07d7f37c9950
rivka-yuliia <juliacreadora@gmail.com>
2023-05-28 20:52:37 +0300

    remove() for BST

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -11,6 +14,24 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def __str__(self):
+        if not self.root:
+            return '{Empty}'
+
+        def traverse(node: Node):
+            tree = dict()
+            tree['value'] = node.value
+            if node.left:
+                tree['left'] = traverse(node.left)
+            if node.right:
+                tree['right'] = traverse(node.right)
+            return tree
+
+        return json.dumps(traverse(self.root))
+
+    def __repr__(self):
+        return self.__str__()
 
     def insert(self, value):
         new_node = Node(value)
@@ -45,13 +66,60 @@ class BinarySearchTree:
                 temp = temp.left
         return None
 
+    def remove(self, value):
+        if not self.root:
+            return False
+        if self.root.value == value:
+            self.root = None
+            return True
+        current = self.root
+        while current:
+            parent = current
+            if value > current.value:
+                current = current.right
+            elif value < current.value:
+                current = current.left
+            else:
+                # Option 1: No right child
+                if not current.right:
+                    if parent.right == current:
+                        parent.right = current.left
+                    elif parent.left == current:
+                        parent.left = current.left
+                    return True
 
-my_tree = BinarySearchTree()
-my_tree.insert(10)
-my_tree.insert(5)
-my_tree.insert(20)
-my_tree.insert(8)
-my_tree.insert(18)
-print(my_tree.lookup(28))
-my_tree.insert(28)
-print(my_tree.lookup(28))
+                # Option 2: Right child that doesn't have a left child
+                if not current.right.left:
+                    current.right.left = current.left
+                    if parent.right == current:
+                        parent.right = current.right
+                    elif parent.left == current:
+                        parent.left = current.right
+                    return True
+
+                # Option 3: Right child that has a left child
+                temp = current.right
+                while temp.left:
+                    parent = temp
+                    temp = temp.left
+                current.value, temp.value = temp.value, current.value
+                if temp.right:
+                    parent.left = temp.right
+                else:
+                    parent.left = None
+                return True
+
+
+my_little_tree = BinarySearchTree()
+my_little_tree.insert(10)
+my_little_tree.insert(5)
+my_little_tree.insert(20)
+my_little_tree.insert(8)
+my_little_tree.insert(18)
+my_little_tree.insert(28)
+my_little_tree.insert(25)
+my_little_tree.insert(30)
+print(my_little_tree)
+my_little_tree.remove(20)
+print(my_little_tree)
+
