8b93295bc62e3904954782dc7b2998a37334a80d
rivka-yuliia <juliacreadora@gmail.com>
2023-05-28 21:40:32 +0300

    fix remove() errors in BST

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -69,42 +69,47 @@ class BinarySearchTree:
     def remove(self, value):
         if not self.root:
             return False
-        if self.root.value == value:
-            self.root = None
-            return True
         current = self.root
+        parent = None
         while current:
-            parent = current
             if value > current.value:
+                parent = current
                 current = current.right
             elif value < current.value:
+                parent = current
                 current = current.left
             else:
                 # Option 1: No right child
                 if not current.right:
-                    if parent.right == current:
+                    if not parent:
+                        self.root = current.left
+                    elif parent.right == current:
                         parent.right = current.left
                     elif parent.left == current:
                         parent.left = current.left
+
                     return True
 
                 # Option 2: Right child that doesn't have a left child
                 if not current.right.left:
                     current.right.left = current.left
-                    if parent.right == current:
+                    if not parent:
+                        self.root = current.right
+                    elif parent.right == current:
                         parent.right = current.right
                     elif parent.left == current:
                         parent.left = current.right
                     return True
 
                 # Option 3: Right child that has a left child
-                temp = current.right
-                while temp.left:
-                    parent = temp
-                    temp = temp.left
-                current.value, temp.value = temp.value, current.value
-                if temp.right:
-                    parent.left = temp.right
+                left_most = current.right.left
+                parent = current.right
+                while left_most.left:
+                    parent = left_most
+                    left_most = left_most.left
+                current.value, left_most.value = left_most.value, current.value
+                if left_most.right:
+                    parent.left = left_most.right
                 else:
                     parent.left = None
                 return True
@@ -120,6 +125,6 @@ my_little_tree.insert(28)
 my_little_tree.insert(25)
 my_little_tree.insert(30)
 print(my_little_tree)
-my_little_tree.remove(20)
+my_little_tree.remove(10)
 print(my_little_tree)
 
