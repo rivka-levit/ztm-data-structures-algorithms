57f5997db2f7bbd6ee82e9be151510661fb0ad99
rivka-yuliia <juliacreadora@gmail.com>
2023-05-30 00:19:39 +0300

    add_edge for Graph

--- graphs/graph_implementation.py
+++ graphs/graph_implementation.py
@@ -6,4 +6,12 @@ class Graph:
     def add_vertex(self, node):
         self.adjacency[node] = list()
 
+    def add_edge(self, node_1, node_2):
+        for node in (node_1, node_2):
+            if node not in self.adjacency:
+                self.add_vertex(node)
+
+        self.adjacency[node_1].append(node_2)
+        self.adjacency[node_2].append(node_1)
+
 
