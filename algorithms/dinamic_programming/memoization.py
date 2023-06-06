cf27c6e6fcf809c31e433e64bc80b053ce9f5b8e
rivka-yuliia <juliacreadora@gmail.com>
2023-06-06 12:37:40 +0300

    fibonacci memoized

--- algorithms/dinamic_programming/memoization.py
+++ algorithms/dinamic_programming/memoization.py
@@ -15,7 +15,17 @@ def add_80(number):
     return number + 80
 
 
+@memoized
+def fib(number):
+    if number == 0:
+        return 0
+    if number == 1:
+        return 1
+    return fib(number-1) + fib(number-2)
+
+
 if __name__ == '__main__':
     print(add_80(5))
     print(add_80(6))
     print(add_80(5))
+    print(fib(9))
