03145fbe33be82320f84c55c6656b3496cd655c9
rivka-yuliia <juliacreadora@gmail.com>
2023-07-17 10:13:53 +0300

    solution

---
+++ leetcode_problems/easy_problems/LL_cycle.py
@@ -0,0 +1,28 @@
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+Full description:
+https://leetcode.com/problems/linked-list-cycle/description/
+"""
+
+
+# Definition for singly-linked list, given in the description.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# My solution with bad space complexity
+def has_cycle(head: ListNode):
+    nodes = set()
+
+    while head:
+        nodes.add(head)
+
+        if head.next in nodes:
+            return True
+
+        head = head.next
+
+    return False
