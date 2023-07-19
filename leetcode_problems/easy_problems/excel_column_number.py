dbd41a5d485591330901279722c2a4f8c64823e6
rivka-yuliia <juliacreadora@gmail.com>
2023-07-19 23:57:24 +0300

    solution

---
+++ leetcode_problems/easy_problems/excel_column_number.py
@@ -0,0 +1,37 @@
+"""
+Given a string columnTitle that represents the column title as appears
+in an Excel sheet, return its corresponding column number.
+
+Full description:
+https://leetcode.com/problems/excel-sheet-column-number/
+"""
+
+
+# My solution
+def title_to_number(columnTitle: str) -> int:
+    chars = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7,
+             'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14,
+             'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20,
+             'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+
+    number = 0
+
+    for ch, pw in zip(columnTitle, list(range(len(columnTitle)-1, -1, -1))):
+        number += chars[ch] * 26 ** pw
+
+    return number
+
+
+# Solution from LeetCode
+# def title_to_number_2(columnTitle: str) -> int:
+#     chars = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7,
+#              'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14,
+#              'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20,
+#              'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+#
+#     number = 0
+#
+#     for power, ch in enumerate(columnTitle[::-1]):
+#         number += chars[ch] * 26 ** power
+#
+#     return number
