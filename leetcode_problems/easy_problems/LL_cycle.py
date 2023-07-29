4e3e559de1c2c1d0e540e00238acf7d8272931fc
rivka-yuliia <juliacreadora@gmail.com>
2023-07-29 23:11:28 +0300

    better solution from LeetCode

--- leetcode_problems/easy_problems/LL_cycle.py
+++ leetcode_problems/easy_problems/LL_cycle.py
@@ -26,3 +26,16 @@ def has_cycle(head: ListNode):
         head = head.next
 
     return False
+
+
+# Solution from LeetCode
+# def has_cycle(head: ListNode):
+#     slow, fast = head, head
+#
+#     while fast and fast.next:
+#         slow = slow.next
+#         fast = fast.next.next
+#         if slow == fast:
+#             return True
+#
+#     return False
