412d401a70fb222e681550746370dd62fa2fc6f6
rivka-yuliia <juliacreadora@gmail.com>
2023-07-30 23:15:09 +0300

    solution O(1)

--- leetcode_problems/medium_problems/min_stack.py
+++ leetcode_problems/medium_problems/min_stack.py
@@ -19,18 +19,24 @@ class MinStack(object):
 
     def __init__(self) -> None:
         self.stack = list()
+        self.min_stack = list()
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val, self.min_stack[-1]))
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def get_min(self) -> int:
-        return min(self.stack)
+        return self.min_stack[-1]
 
 
 min_stack = MinStack()
