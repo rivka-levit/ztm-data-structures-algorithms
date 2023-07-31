0f0a16ad2c821c44fc92080302c5b95d35218c34
rivka-yuliia <juliacreadora@gmail.com>
2023-07-31 09:01:48 +0300

    solution O(1) space complexity

--- leetcode_problems/easy_problems/intersection_two_LL.py
+++ leetcode_problems/easy_problems/intersection_two_LL.py
@@ -15,6 +15,7 @@ class ListNode(object):
         self.next = None
 
 
+# My solution O(n) space complexity
 def get_intersection_node(headA: ListNode, headB: ListNode) -> ListNode | None:
     intersect = None
     visited = set()
@@ -30,3 +31,21 @@ def get_intersection_node(headA: ListNode, headB: ListNode) -> ListNode | None:
         headB = headB.next
 
     return intersect
+
+
+# Solution from LeetCode O(1) space complexity
+# def get_intersection_node(headA: ListNode, headB: ListNode) -> ListNode | None:
+#     p1 = headA
+#     p2 = headB
+#
+#     while p1 != p2:
+#         if p1 is None:
+#             p1 = headB
+#         else:
+#             p1 = p1.next
+#         if p2 is None:
+#             p2 = headA
+#         else:
+#             p2 = p2.next
+#
+#     return p1
